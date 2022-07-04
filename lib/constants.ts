590d05f70d24580023a32ba47a9950a2297092ac
Mike MacCana <mike.maccana@gmail.com>
2022-07-04 16:28:48 +0100

    Add constants

---
+++ lib/constants.ts
@@ -0,0 +1,30 @@
+import { clusterApiUrl } from "@solana/web3.js";
+
+export const URLS: Record<string, string> = {
+  // https://solanacookbook.com/references/local-development.html#starting-a-local-validator
+  devNet: clusterApiUrl("devnet"),
+  mainNetBeta: clusterApiUrl("mainnet-beta"),
+  testNet: clusterApiUrl("testnet"),
+  // https://www.quicknode.com/endpoints/49328
+  quickNodeDevNet:
+    "wss://red-aged-dream.solana-devnet.quiknode.pro/cb0472330c14ad913e8a4430c70c182570f96047/",
+  // https://shdw.genesysgo.com/genesysgo/the-genesysgo-rpc-network
+  // The SSC DAO network handles both https and wss connections. It provides unlimited data, global DNS load balancing for the lowest latency possible, backed by 300+ bare metal servers in 9 countries across 3 different continents. Additionally, the SSC DAO network has the full ledger history back to the genesis block.
+  genesysGoMain: "https://ssc-dao.genesysgo.net/",
+  genesysGoDevNet: "https://devnet.genesysgo.net/",
+};
+
+// BIP44
+// From https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#purpose
+export const PURPOSE = 44;
+
+// From https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+export const SOLANA_COIN_TYPE = 501;
+
+// Per https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#change
+// Constant 0 is used for external chain and constant 1 for internal chain (also known as change addresses). External chain is used for addresses that are meant to be visible outside of the wallet (e.g. for receiving payments).
+export const EXTERNAL_CHAIN = 0;
+
+// From https://solana-labs.github.io/solana-web3.js/classes/Keypair.html#fromSeed
+// 'Generate a keypair from a 32 byte seed.'
+export const SOLANA_SEED_SIZE_BYTES = 32;
