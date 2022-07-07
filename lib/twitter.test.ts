0534f096d7a607f1aa5a4b21cda50faf4d394587
Mike MacCana <mike.maccana@gmail.com>
2022-07-07 14:00:16 +0100

    Minor cleanups

--- lib/twitter.test.ts
+++ lib/twitter.test.ts
@@ -2,8 +2,10 @@ import { getHandleAndRegistryKeyForWallet } from "./twitter";
 // Shaq is one of the only people I know
 // with a verified Twitter handle and a Solana wallet.
 const SHAQS_WALLET = "gacMrsrxNisAhCfgsUAVbwmTC3w9nJB6NychLAnTQFv";
-const JOE_MACCANS_WALLET = "5CJFJoKiZ14tdsjtWgKGQVuVkYW7pcWUR4LFSvDELFod";
+const JOE_MACCANNS_WALLET = "5CJFJoKiZ14tdsjtWgKGQVuVkYW7pcWUR4LFSvDELFod";
 const MIKES_WALLET = "5FHwkrdxntdK24hgQU8qgBjn35Y1zwhz1GZwCkP2UJnM";
+
+// Also tried 'raj', 'joker', 'hge' - nobody has reverse records to match Twitter to therr identity
 
 describe("Twitter verification", () => {
   test(`Says Shaq's wallet hasn't been set up for reverse Twitter`, async () => {
@@ -14,17 +16,14 @@ describe("Twitter verification", () => {
 
   test(`Says Joe McCann's wallet hasn't been set up for reverse Twitter`, async () => {
     expect(
-      getHandleAndRegistryKeyForWallet(JOE_MACCANS_WALLET)
+      getHandleAndRegistryKeyForWallet(JOE_MACCANNS_WALLET)
     ).rejects.toThrow("Invalid reverse Twitter account provided");
   });
 
-  test(`Says Mike's wallet is verified`, async () => {
-    const [handle, registry] = await getHandleAndRegistryKeyForWallet(
+  test(`Says Mike's wallet belongs to Mike's (unverified) twitter handle`, async () => {
+    const [handle, _registry] = await getHandleAndRegistryKeyForWallet(
       MIKES_WALLET
     );
     expect(handle).toEqual("mikemaccana");
-    expect(registry.toString()).toEqual(
-      "3NWbxL6YE2YYBeTXUXznmKRzkwqrFvhSnYwnf5nhJRSR"
-    );
   });
 });
