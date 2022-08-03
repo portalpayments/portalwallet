c39944261bd759375df0a58290c9d8841b6aa3e0
Mike MacCana <mike.maccana@gmail.com>
2022-08-03 15:16:12 +0100

    Add nav area

--- vite-project/src/lib/Wallet.svelte
+++ vite-project/src/lib/Wallet.svelte
@@ -9,13 +9,18 @@
   <div class="features">
     <div class="feature transfer">
       transfer
-      <button on:click={increment}>
+      <button class="increment-count" on:click={increment}>
         count is {count}
       </button>
     </div>
     <div class="feature contacts">contacts</div>
     <div class="feature collectibles">collectibles</div>
   </div>
+  <nav>
+    <button class="navitem">icon</button>
+    <button class="navitem">icon</button>
+    <button class="navitem">icon</button>
+  </nav>
 </div>
 
 <style>
@@ -25,12 +30,21 @@
     box-sizing: border-box;
   }
 
+  /* The entire wallet, contains the sliding main area plus the nav bar */
   .wallet {
     width: 300px;
     height: 600px;
     overflow: hidden;
+    grid-auto-flow: row;
+    grid-template-rows: 1fr 48px;
   }
 
+  .increment-count {
+    width: 20px;
+    height: 20px;
+  }
+
+  /* The sliding area with each of our features (wider than our actual app)*/
   .features {
     background-color: #ccc;
     width: calc(3 * 300px);
@@ -42,9 +56,14 @@
   .feature {
     background-color: #999;
   }
+  nav {
+    width: 100%;
+    grid-auto-flow: row;
+    height: 24px;
+    grid-auto-flow: column;
+  }
 
-  button {
+  nav button {
     width: 20px;
-    height: 20px;
   }
 </style>
