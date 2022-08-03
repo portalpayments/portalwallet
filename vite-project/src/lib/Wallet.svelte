86575b22a235e01238ec1419e8e50827dcd1d9d9
Mike MacCana <mike.maccana@gmail.com>
2022-08-03 15:32:28 +0100

    Add transfer.svg

--- vite-project/src/lib/Wallet.svelte
+++ vite-project/src/lib/Wallet.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-  let count: number = 0;
-  const increment = () => {
-    count += 1;
+  const log = console.log;
+  let currentFeature: number = 0;
+  const navigate = (newFeature) => {
+    log(`Changing currentFeature to ${newFeature}`);
+    currentFeature += newFeature;
   };
 </script>
 
 <div class="wallet">
   <div class="features">
-    <div class="feature transfer">
-      transfer
-      <button class="increment-count" on:click={increment}>
-        count is {count}
-      </button>
-    </div>
+    <div class="feature transfer">transfer</div>
     <div class="feature contacts">contacts</div>
     <div class="feature collectibles">collectibles</div>
   </div>
   <nav>
-    <button class="navitem">icon</button>
-    <button class="navitem">icon</button>
-    <button class="navitem">icon</button>
+    <button class="transfer" on:click={() => navigate(0)}>icon</button>
+    <button class="contacts" on:click={() => navigate(1)}>icon</button>
+    <button class="collectibles" on:click={() => navigate(2)}>icon</button>
   </nav>
 </div>
 
 <style>
+  :root {
+    --wallet-height: 600px;
+    --wallet-width: 300px;
+  }
   * {
     display: grid;
     background-color: white;
@@ -32,8 +33,8 @@
 
   /* The entire wallet, contains the sliding main area plus the nav bar */
   .wallet {
-    width: 300px;
-    height: 600px;
+    width: var(--wallet-width);
+    height: var(--wallet-height);
     overflow: hidden;
     grid-auto-flow: row;
     grid-template-rows: 1fr 48px;
@@ -47,7 +48,7 @@
   /* The sliding area with each of our features (wider than our actual app)*/
   .features {
     background-color: #ccc;
-    width: calc(3 * 300px);
+    width: calc(3 * var(--wallet-width));
     grid-auto-flow: column;
     gap: 12px;
     padding: 12px;
@@ -57,13 +58,17 @@
     background-color: #999;
   }
   nav {
-    width: 100%;
+    width: var(--wallet-width);
     grid-auto-flow: row;
-    height: 24px;
     grid-auto-flow: column;
   }
 
   nav button {
     width: 20px;
+    color: black;
+  }
+
+  nav .transfer {
+    background-image: url("assets/transfer.svg");
   }
 </style>
