20a1be0823e2acc1ad8f82d9dcc017d8f96d25de
Mike MacCana <mike.maccana@gmail.com>
2022-08-04 11:21:17 +0100

    Iterate over transactions

--- vite-project/src/lib/Transactions.svelte
+++ vite-project/src/lib/Transactions.svelte
@@ -1,24 +1,42 @@
 <script>
   import avatarImageUrl from "../assets/avatar.png";
   console.log("Hello from transactions component");
+
+  let transactions = [
+    {
+      image: avatarImageUrl,
+      name: "John O'Hara",
+      isPositive: true,
+      amountMajor: 400,
+      amountMinor: 0,
+    },
+    {
+      image: avatarImageUrl,
+      name: "ProteinLand",
+      isPositive: false,
+      amountMajor: 3,
+      amountMinor: 50,
+    },
+    {
+      image: avatarImageUrl,
+      name: "Jane Taylor",
+      isPositive: false,
+      amountMajor: 21,
+      amountMinor: 25,
+    },
+  ];
 </script>
 
 <div class="transactions">
-  <div class="transaction">
-    <img src={avatarImageUrl} alt="" />
-    <div class="name">name</div>
-    <div class="amount">amount</div>
-  </div>
-  <div class="transaction">
-    <img src={avatarImageUrl} alt="" />
-    <div class="name">name</div>
-    <div class="amount">amount</div>
-  </div>
-  <div class="transaction">
-    <img src={avatarImageUrl} alt="" />
-    <div class="name">name</div>
-    <div class="amount">amount</div>
-  </div>
+  {#each transactions as transaction, index}
+    <div class="transaction">
+      <img src={transaction.image} alt="" />
+      <div class="name">{transaction.name}</div>
+      <div class="amount">
+        {transaction.amountMajor}.{transaction.amountMinor}
+      </div>
+    </div>
+  {/each}
 </div>
 
 <style>
