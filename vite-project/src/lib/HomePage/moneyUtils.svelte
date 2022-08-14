218f4146e487660abd8bcf6380298be8c7799675
bluechipsprinter <vaheh.hatami@gmail.com>
2022-08-14 06:00:19 +0200

    changed the buttons to Links in  moneyUtils forr a smoother routing experience

--- vite-project/src/lib/HomePage/moneyUtils.svelte
+++ vite-project/src/lib/HomePage/moneyUtils.svelte
@@ -1,40 +1,47 @@
 <script lang="ts">
   import { Link } from "svelte-navigator";
-  const log = console.log;
-  const addMoney = () => {
-    window.location.assign("/addMoneyToAccount");
-  };
-
-  const transferMoney = () => {
-    window.location.assign("/transferMoney");
-  };
 </script>
 
 <div class="MoneyUtilButtons">
-  <button on:click={() => addMoney()}>add money</button>
-  <button on:click={() => transferMoney()}>transfer</button>
+  <!-- <button on:click={() => addMoney()}>add money</button> -->
+  <Link class="Link" to="/addMoneyToAccount">add money</Link>
+  <Link to="/transferMoney">transfer money</Link>
+  <!-- <button on:click={() => transferMoney()}>transfer</button> -->
 </div>
 
 <style>
   .MoneyUtilButtons {
     display: grid;
     grid-auto-flow: column;
-    grid-template-columns: 40% 40%;
-    gap: 10px;
+    grid-template-columns: 43% 43%;
+    gap: 6px;
     justify-content: center;
     align-items: center;
   }
 
-  .MoneyUtilButtons button {
+  .Link > :global(a) {
+    text-decoration: none;
+  }
+  :global(a) {
     color: #2775c9;
     font-weight: 600;
     background-color: #419cfd33;
     padding: 7px 0px;
     border-radius: 8px;
-    font-size: 1.1rem;
+    font-size: 0.97rem;
   }
 
-  .MoneyUtilButtons button:active {
+  :global(a):link {
+    text-decoration: none;
+  }
+  :global(a):visited {
+    text-decoration: none;
+  }
+  :global(a):hover {
+    text-decoration: none;
+  }
+  :global(a):active {
+    text-decoration: none;
     color: #fff;
     background-color: #419cfd;
   }
