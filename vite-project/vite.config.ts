e0dc22b4ed05222295676e92351a1ebc1ced5acf
Mike MacCana <mike.maccana@gmail.com>
2022-08-22 14:58:46 +0100

    Use newer vite version

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { build, defineConfig, optimizeDeps } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+// See https://github.com/sveltejs/kit/issues/859
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+  build: {
+    target: "es2020",
+  },
+});
