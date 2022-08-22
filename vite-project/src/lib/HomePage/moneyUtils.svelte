24619f356a776691f5cd336703a05331ee74e1f3
Mike MacCana <mike.maccana@gmail.com>
2022-08-22 17:07:17 +0100

    Remove unused code causing warnings

--- vite-project/src/lib/HomePage/moneyUtils.svelte
+++ vite-project/src/lib/HomePage/moneyUtils.svelte
@@ -18,31 +18,4 @@
     justify-content: center;
     align-items: center;
   }
-
-  .link > :global(a) {
-    text-decoration: none;
-  }
-  :global(a) {
-    color: #2775c9;
-    font-weight: 600;
-    background-color: #419cfd33;
-    padding: 7px 0px;
-    border-radius: 8px;
-    font-size: 0.97rem;
-  }
-
-  :global(a) {
-    text-decoration: none;
-  }
-  :global(a):visited {
-    text-decoration: none;
-  }
-  :global(a):hover {
-    text-decoration: none;
-  }
-  :global(a):active {
-    text-decoration: none;
-    color: #fff;
-    background-color: #419cfd;
-  }
 </style>
