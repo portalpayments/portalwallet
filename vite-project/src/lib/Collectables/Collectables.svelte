bd1082cb9dae48a7090ab4881445d9ea12f6814c
Mike MacCana <mike.maccana@gmail.com>
2022-08-22 13:51:06 +0100

    Dynamically list NFTs per TODO

--- vite-project/src/lib/Collectables/Collectables.svelte
+++ vite-project/src/lib/Collectables/Collectables.svelte
@@ -2,14 +2,27 @@
   import beepleMockNFT from "../../assets/Collectables/beepleMockNFT.jpg";
   import beepleMockNFT2 from "../../assets/Collectables/beepleMockNFT2.jpg";
   import baycMockNFT from "../../assets/Collectables/baycMockNFT.jpg";
+
+  const collectables = [
+    {
+      image: beepleMockNFT,
+      description: "beeple's Spaceman on a new planet",
+    },
+    {
+      image: baycMockNFT,
+      description: "BAYC NFT",
+    },
+    {
+      image: beepleMockNFT2,
+      description: "beeple's human production machine",
+    },
+  ];
 </script>
 
-<!-- TODO create a dynamic way of listing NFTs, by looping over an array of owned NFTs -->
-
 <div class="nfts">
-  <img src={beepleMockNFT} alt="beeple\'s Spaceman on a new planet" />
-  <img src={baycMockNFT} alt="BAYC NFT" />
-  <img src={beepleMockNFT2} alt="beeple\'s human poduction machine" />
+  {#each collectables as collectable}
+    <img src={collectable.image} alt={collectable.description} />
+  {/each}
 </div>
 
 <style>
