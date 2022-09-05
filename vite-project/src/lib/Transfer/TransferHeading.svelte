e8a7f5d68f9436e0ba1ddd5392c9e243ed0bfc2a
Mike MacCana <mike.maccana@gmail.com>
2022-09-05 16:18:26 +0100

    Try colored background techique used on website

--- vite-project/src/lib/Transfer/TransferHeading.svelte
+++ vite-project/src/lib/Transfer/TransferHeading.svelte
@@ -52,6 +52,17 @@
     font-size: 24px;
     /* Center, despite back button */
     transform: translateX(-12px);
+
+    /* Colored background techique used on website */
+    color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+    background-image: linear-gradient(
+      350deg,
+      var(--dark-blue),
+      var(--teal),
+      var(--bright-green)
+    );
   }
 
   .verification-status {
