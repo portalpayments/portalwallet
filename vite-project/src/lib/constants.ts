da15203ac495d8db665868db47e99f676dc36f83
Mike MacCana <mike.maccana@gmail.com>
2022-09-10 16:21:54 +0100

    Smaller warning message

--- vite-project/src/lib/constants.ts
+++ vite-project/src/lib/constants.ts
@@ -10,7 +10,7 @@ export const identityTokenIssuerPublicKeyString =
   "FSVgrW58amFmH91ZKBic686qVhHayMt3wS8bCpisUph9";
 
 export const warningUnverifiedAccount =
-  "We suggest you only send money to verified addresses. You can click below to request verification. We will notify you once the address is verified.";
+  "Only send money to verified recipients. We will notify you once the address is verified.";
 
 // Used to validate 12, 12.34, etc.
 export const NUMBERS_OPTIONAL_DECIMAL_PLACE_TWO_NUMBERS = "^d*(.d{0,2})?$";
