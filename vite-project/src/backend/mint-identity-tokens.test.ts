c2c4c90a268e4d81646551f552c13b7decae59a3
Mike MacCana <mike.maccana@gmail.com>
2022-09-13 10:54:20 +0100

    Skip test only used for YC demo

--- vite-project/src/backend/mint-identity-tokens.test.ts
+++ vite-project/src/backend/mint-identity-tokens.test.ts
@@ -1,14 +1,19 @@
 import { log } from "./functions";
-import { YCOMBINATOR_DEMO_WALLET_FOR_JARED } from "./constants"
-import { mintAndTransferIdentityToken } from "./mint-identity-tokens"
+import { YCOMBINATOR_DEMO_WALLET_FOR_JARED } from "./constants";
+import { mintAndTransferIdentityToken } from "./mint-identity-tokens";
 
 describe(`mint an identity token`, () => {
-  test(`can mint an identity token for YC demo`, async () => {
+  // Only needed for the YC demo
+  // (would normally be a script but I couldn't get ts-node
+  // to work properly)
+  test.skip(`can mint an identity token for YC demo`, async () => {
+    const transactionId = await mintAndTransferIdentityToken(
+      YCOMBINATOR_DEMO_WALLET_FOR_JARED,
+      "Jared",
+      "Friedman"
+    );
+    log(transactionId);
 
-    const transactionId = await mintAndTransferIdentityToken(YCOMBINATOR_DEMO_WALLET_FOR_JARED, "Jared", "Friedman");
-    log(transactionId)
-
-    
     log(`✅ Completed successfully`);
-  })
-})
+  });
+});
