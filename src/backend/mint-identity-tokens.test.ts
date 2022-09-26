a84620f51950421d823dfe936b521a289f5db711
Mike MacCana <mike.maccana@gmail.com>
2022-09-26 17:48:52 +0100

    Mint token for Joe McCann

--- src/backend/mint-identity-tokens.test.ts
+++ src/backend/mint-identity-tokens.test.ts
@@ -1,19 +1,28 @@
 import { log } from "./functions";
-import { YCOMBINATOR_DEMO_WALLET_FOR_JARED } from "./constants";
+import {
+  JOE_MCCANNS_WALLET,
+  MINUTE,
+  MINUTES,
+  YCOMBINATOR_DEMO_WALLET_FOR_JARED,
+} from "./constants";
 import { mintAndTransferIdentityToken } from "./mint-identity-tokens";
 
 describe(`mint an identity token`, () => {
   // Only needed for the YC demo
   // (would normally be a script but I couldn't get ts-node
   // to work properly)
-  test.skip(`can mint an identity token for YC demo`, async () => {
-    const transactionId = await mintAndTransferIdentityToken(
-      YCOMBINATOR_DEMO_WALLET_FOR_JARED,
-      "Jared",
-      "Friedman"
-    );
-    log(transactionId);
+  test.skip(
+    `can mint a real identity token for demo purposes`,
+    async () => {
+      const transactionId = await mintAndTransferIdentityToken(
+        JOE_MCCANNS_WALLET,
+        "Joseph Isaac",
+        "McCann"
+      );
+      log(transactionId);
 
-    log(`✅ Completed successfully`);
-  });
+      log(`✅ Completed successfully`);
+    },
+    3 * MINUTES
+  );
 });
