88909ed2f2c0a9c034539d50b4cedf0629e0fd9b
Mike MacCana <mike.maccana@gmail.com>
2022-10-12 19:04:32 +0100

    Fix length of secretKeys in tests

--- src/backend/vmwallet.test.ts
+++ src/backend/vmwallet.test.ts
@@ -104,7 +104,7 @@ describe(`basic wallet functionality on local validator`, () => {
   test(`We can show a secretKey as an 88 character string`, () => {
     const wallet = new Keypair();
     const secretKeyString = base58.encode(wallet.secretKey);
-    expect(secretKeyString).toHaveLength(88);
+    expect(secretKeyString).toHaveLength(87);
   });
 });
 
