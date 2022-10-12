9cb1b26b61c91f0147c954d9a8d0733a376e2730
Mike MacCana <mike.maccana@gmail.com>
2022-10-12 15:41:03 +0100

    Add IS_INVESTOR_DEMO variable

--- src/App.svelte
+++ src/App.svelte
@@ -31,14 +31,18 @@
 
   let user: null | User = null;
 
+  const IS_INVESTOR_DEMO = false;
+
   // Hack for demo wallet
   (async () => {
-    await saveSettingsHack(
-      // Secret key for JOHN_TESTUSER_DEMO_WALLET
-      "HyLLb7PQd5uWz9m8ME7j177gDDJeLwPuYSApd1FhbHB4f1udTJ5thfSXnm2MXsdHGYp7DTLGgZsxdAqtoEohmsu",
-      "password"
-    );
-    log(`Demo wallet for test user has been set up`);
+    if (IS_INVESTOR_DEMO) {
+      await saveSettingsHack(
+        // Secret key for JOHN_TESTUSER_DEMO_WALLET
+        "HyLLb7PQd5uWz9m8ME7j177gDDJeLwPuYSApd1FhbHB4f1udTJ5thfSXnm2MXsdHGYp7DTLGgZsxdAqtoEohmsu",
+        "password"
+      );
+      log(`Demo wallet for test user has been set up`);
+    }
   })();
 
   connectionStore.subscribe((newValue) => {
