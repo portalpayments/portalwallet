91dcedc49ea92682e69022adc63966c7a331342d
Mike MacCana <mike.maccana@gmail.com>
2022-10-15 19:25:31 +0100

    Remove dead code

--- src/backend/node-functions.ts
+++ src/backend/node-functions.ts
@@ -1,10 +1,5 @@
 import { promisify } from "util";
 import { inspect as originalInspect } from "util";
-import { scrypt as scryptCallback } from "crypto";
-
-// Convert `scrypt()` into a function that takes the
-// same parameters but returns a promise.
-export const scrypt = promisify(scryptCallback);
 
 export const inspect = (object: any) => {
   return originalInspect(object, true, null, true);
