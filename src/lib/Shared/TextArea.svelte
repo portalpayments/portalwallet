c034f4cf8372bb876718ed673f88ead5c37c2b41
Mike MacCana <mike.maccana@gmail.com>
2022-10-19 18:00:15 +0100

    Add a gradient border to textarea

--- src/lib/Shared/TextArea.svelte
+++ src/lib/Shared/TextArea.svelte
@@ -21,28 +21,72 @@
   };
 </script>
 
-<textarea
-  {placeholder}
-  on:input|preventDefault={debounce(runInputDelayIfExists)}
-  on:keyup|preventDefault={debounce(runKeyUpDelayIfExists)}
-/>
+<div class="gradient-box">
+  <textarea
+    {placeholder}
+    on:input|preventDefault={debounce(runInputDelayIfExists)}
+    on:keyup|preventDefault={debounce(runKeyUpDelayIfExists)}
+  />
+</div>
 
 <style>
+  /* Use gradients for borders */
+  /* From https://css-tricks.com/gradient-borders-in-css/ */
+  /* Note we cannot use a :before element on the textarea itself */
+  /* See https://stackoverflow.com/questions/3286991/can-a-before-selector-be-used-with-a-textarea */
+
+  :root {
+    --thickness: 3px;
+  }
+
+  /* Box that has a transparent border */
+  .gradient-box {
+    position: relative;
+    border: var(--thickness);
+    color: white;
+    background-clip: padding-box;
+    border: solid var(--thickness) transparent;
+    border-radius: calc(2 * var(--thickness));
+  }
+
+  /* Big outer box with a gradient, shown beneath */
+  .gradient-box:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: calc(-1 * var(--thickness));
+    border-radius: inherit;
+    background: var(--blue-green-gradient);
+  }
+
   textarea {
-    border-radius: 7px;
     width: 100%;
-    background-color: white;
-    padding: 6px;
+    border-radius: var(--thickness);
+    padding: var(--thickness);
     height: 150px;
     resize: none;
     color: var(--actually-dark-grey);
+    background-color: white;
+    outline: none;
+    border: solid 6px transparent;
+    position: relative;
   }
 
-  textarea:focus {
-    outline: none;
-    /* TODO: gradient for border? */
-    border: 2px solid var(--mid-blue);
-    box-shadow: 0 0 2px rgb(65 156 253 / 30%);
+  textarea:before {
+    content: "SWAG";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -5px;
+    border-radius: inherit;
+    background: linear-gradient(to right, red, orange);
   }
 
   textarea::placeholder {
