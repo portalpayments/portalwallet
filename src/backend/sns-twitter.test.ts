e82282eb4ae4dd9a3af7c9d961ee2e4ff9dd78c7
Mike MacCana <mike.maccana@gmail.com>
2022-11-16 15:18:04 +0000

    x

---
+++ src/backend/sns-twitter.test.ts
@@ -0,0 +1,69 @@
+import { getTwitterRegistry } from "@bonfida/spl-name-service";
+import { Connection } from "@solana/web3.js";
+import {
+  JOE_MCCANNS_WALLET,
+  MIKES_WALLET,
+  SHAQS_WALLET,
+  URLS,
+} from "./constants";
+import {
+  getHandleForWallet,
+  checkIsVerified,
+  checkIsWalletVerifiedViaTwitter,
+  closeConnection,
+} from "./sns-twitter";
+
+// Also tried 'raj', 'joker', 'hge' - nobody has reverse records to match Twitter to therr identity
+
+describe("Wallet to Twitter mapping", () => {
+  test(`Says Shaq's wallet hasn't been set up for reverse Twitter`, async () => {
+    expect(await getHandleForWallet(SHAQS_WALLET)).toBeNull();
+  });
+
+  test(`Says Joe McCann's wallet hasn't been set up for reverse Twitter`, async () => {
+    expect(await getHandleForWallet(JOE_MCCANNS_WALLET)).toBeNull();
+  });
+
+  test(`Shows mapping to Mike's Twitter account`, async () => {
+    expect(await getHandleForWallet(MIKES_WALLET)).toEqual("mikemaccana");
+  });
+});
+
+// Skip to ignore TLS errors
+// https://github.com/PLhery/node-twitter-api-v2/issues/326
+describe.skip(`Twitter verification`, () => {
+  afterAll(() => {
+    closeConnection();
+  });
+
+  test(`Says @mikemaccana's is not verified`, async () => {
+    expect(await checkIsVerified("mikemaccana")).toBeFalsy();
+  });
+
+  test(`Says @joemccann's is verified`, async () => {
+    expect(await checkIsVerified("joemccann")).toBeTruthy();
+  });
+});
+
+describe(`Twitter name to wallet look up`, () => {
+  test(`Finds @mikemaccana's wallet`, async () => {
+    const connection = new Connection(URLS.mainNetBeta);
+    const registry = await getTwitterRegistry(connection, "mikemaccana");
+    expect(registry.owner.toString()).toEqual(MIKES_WALLET);
+  });
+});
+
+describe(`Wallet => Twitter verification`, () => {
+  afterAll(() => {
+    closeConnection();
+  });
+  // See link above re: TLS error
+  test.skip(`Mike isn't verified`, async () => {
+    expect(await checkIsWalletVerifiedViaTwitter(MIKES_WALLET)).toBeFalsy();
+  });
+  test(`Joe doesn't have wallet to Twitter mappings set up`, async () => {
+    expect(
+      await checkIsWalletVerifiedViaTwitter(JOE_MCCANNS_WALLET)
+    ).toBeFalsy();
+  });
+});
---
+++ src/backend/sns-twitter.ts
@@ -0,0 +1,70 @@
+import { PublicKey, Connection } from "@solana/web3.js";
+import { getHandleAndRegistryKey } from "@bonfida/spl-name-service";
+import { URLS } from "./constants";
+import { TwitterApi, TwitterApiReadOnly } from "twitter-api-v2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let _twitterClient: TwitterApiReadOnly | null;
+
+export const getHandleForWallet = async (wallet: string) => {
+  const connection = new Connection(URLS["mainNetBeta"]);
+  // Pubkey of the wallet you want to retrieve the Twitter handle
+  const pubkey = new PublicKey(wallet);
+
+  try {
+    const [handle, _RegistryKey] = await getHandleAndRegistryKey(
+      connection,
+      pubkey
+    );
+
+    return handle;
+  } catch (thrownObject) {
+    const error = thrownObject as Error;
+    // They SNS user just doesn't have a Twitter reverse mapping set up
+    // This is super common
+    if (error.message === "Invalid reverse Twitter account provided") {
+      return null;
+    }
+    // An unexpected error
+    throw error;
+  }
+};
+
+export const checkIsVerified = async (handle: string) => {
+  // Instantiate with desired auth type (here's Bearer v2 auth)
+  if (!_twitterClient) {
+    let bearerToken = process.env.TWITTER_API_KEY_BEARER_TOKEN || null;
+    if (!bearerToken) {
+      throw new Error(
+        `Please set TWITTER_API_KEY_BEARER_TOKEN (see README.md for details)`
+      );
+    }
+    // Tell typescript it's a readonly app
+    _twitterClient = new TwitterApi(bearerToken).readOnly;
+  }
+
+  // Play with the built in methods
+  const user = await _twitterClient.v2.userByUsername(handle, {
+    "user.fields": "verified",
+  });
+  return user.data.verified || null;
+};
+
+export const checkIsWalletVerifiedViaTwitter = async (wallet: string) => {
+  const handle = await getHandleForWallet(wallet);
+  if (!handle) {
+    return null;
+  }
+  return checkIsVerified(handle);
+};
+
+export const closeConnection = async () => {
+  if (!_twitterClient) {
+    return;
+  }
+  // TODO: close socket when issue below is resolved
+  // See https://github.com/PLhery/node-twitter-api-v2/issues/326
+  return;
+};
