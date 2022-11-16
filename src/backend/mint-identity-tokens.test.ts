843038481cd28a3025a9ae3a34ba137f08388124
Mike MacCana <mike.maccana@gmail.com>
2022-11-16 11:25:05 +0000

    Use more variables

--- src/backend/mint-identity-tokens.test.ts
+++ src/backend/mint-identity-tokens.test.ts
@@ -8,14 +8,23 @@ describe(`mint an identity token`, () => {
   // Only needed for the investor demo
   // (would normally be a script but I couldn't get ts-node
   // to work properly)
+  //
+  // DO NOT RE-ENABLE THIS TEST AND COMMIT THAT CHANGE
+  // (IT IS A HACK WHILE I GET TS-NODE WORKING)
+  //
+  const WALLET_ADDRESS = JOHN_TESTUSER_DEMO_WALLET;
+  const firstName = "John";
+  const lastName = "Testuser";
+
   test.skip(
     `can mint a real identity token for demo purposes`,
+
     async () => {
       log(`🎫 Minting identity token...`);
       const transactionId = await mintAndTransferIdentityToken(
-        JOHN_TESTUSER_DEMO_WALLET,
-        "John",
-        "Testuser"
+        WALLET_ADDRESS,
+        firstName,
+        lastName
       );
       log(transactionId);
 
