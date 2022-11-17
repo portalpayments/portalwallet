1f2ad6187869249e68adc4441b1b4b4c4cdd1197
Mike MacCana <mike.maccana@gmail.com>
2022-11-17 12:44:48 +0000

    Comments only

--- src/backend/mint-identity-tokens.test.ts
+++ src/backend/mint-identity-tokens.test.ts
@@ -4,19 +4,15 @@ import { mintAndTransferIdentityToken } from "./mint-identity-tokens";
 
 log(`Running token minter...`);
 
+// Only needed for alpha tests
+// (would normally be a script not a test but I couldn't get ts-node to work properly)
 describe(`mint a real identity token`, () => {
-  // Only needed for the investor demo
-  // (would normally be a script but I couldn't get ts-node
-  // to work properly)
-  //
-  // DO NOT RE-ENABLE THIS TEST AND COMMIT THAT CHANGE
-  // (IT IS A HACK WHILE I GET TS-NODE WORKING)
-  //
-
   const WALLET_ADDRESS = "";
   const firstName = "";
   const lastName = "";
 
+  // DO NOT RE-ENABLE THIS TEST AND COMMIT THAT CHANGE
+  // (IT IS A HACK WHILE I GET TS-NODE WORKING)
   test.skip(
     `can mint a real identity token for demo purposes`,
 
