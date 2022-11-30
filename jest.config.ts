3716b956b1ec8514dda7f2721302b660836ecf2b
Mike MacCana <mike.maccana@gmail.com>
2022-11-30 10:32:53 +0000

    fix jest complaining about SVG files

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
   // Note this causes Jest to always exit successfully, which sucks, but it beats two pages of error messages whenever we run tests.
   // From https://github.com/visionmedia/supertest/issues/520#issuecomment-909989004
   globalTeardown: "./test-teardown-globals.ts",
+
+  // From https://stackoverflow.com/questions/58603201/jest-cannot-load-svg-file
+  transform: {
+    "^.+\\.svg$": "<rootDir>/svgTransform.ts",
+  },
 };
 
 export default config;
