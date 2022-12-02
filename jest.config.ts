17af6f8010c0d03ef921e655dbf15151ff09256b
Mike MacCana <mike.maccana@gmail.com>
2022-12-02 23:10:46 +0000

    Remove hack for open file handles issue - just deal with errors

--- jest.config.ts
+++ jest.config.ts
@@ -7,11 +7,6 @@ const config: Config = {
   // Oddly this doesn't detect any open handles, but it does stop the message.
   // TODO: fix when we can be bothered.
   detectOpenHandles: true,
-  // Workaround for open handles - https://github.com/solana-labs/solana/issues/27464
-  // and https://solana.stackexchange.com/questions/1685/how-do-i-prevent-open-handles-issues-when-using-the-solana-connection-object
-  // Note this causes Jest to always exit successfully, which sucks, but it beats two pages of error messages whenever we run tests.
-  // From https://github.com/visionmedia/supertest/issues/520#issuecomment-909989004
-  globalTeardown: "./test-teardown-globals.ts",
 
   // From https://stackoverflow.com/questions/58603201/jest-cannot-load-svg-file
   transform: {
--- test-teardown-globals.ts
+++
@@ -1,4 +0,0 @@
-export default () => {
-  // See jest.config.js
-  process.exit(0);
-};
