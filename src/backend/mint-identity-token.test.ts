d58d634f2a025b85b9d25853ac2715ff4c9a00c4
Mike MacCana <mike.maccana@gmail.com>
2022-12-02 22:29:11 +0000

    Add new identity token minter

---
+++ src/backend/mint-identity-token.test.ts
@@ -0,0 +1,59 @@
+const testOrSkip = process.env.RUN_SKIPPED_TESTS ? test : test.skip;
+
+// Run with 'npx tsx mint-identity-token.ts'
+// because 'ts-node' has issues:
+// https://github.com/TypeStrong/ts-node/issues/1062#issuecomment-1192847985
+
+import { log, sleep, stringify } from "./functions";
+import { mintAndTransferIdentityToken } from "./identity-tokens";
+import { uploadImageToArweave } from "./arweave";
+import dotenv from "dotenv";
+import { getKeypairFromString } from "./vmwallet";
+import { SECOND, SECONDS } from "./constants";
+
+dotenv.config();
+
+const identityTokenSecretKey = process.env.IDENTITY_TOKEN_SECRET_KEY;
+
+if (!identityTokenSecretKey) {
+  throw new Error(`Please set IDENTITY_TOKEN_SECRET_KEY in .env file`);
+}
+
+const WALLET_ADDRESS = "";
+const GIVEN_NAME = "";
+const FAMILY_NAME = "";
+const IMAGE_FILE = "";
+
+testOrSkip(
+  "Mints a real identity token",
+  async () => {
+    log(`🎟️ Running Portal Identity token minter ...`);
+
+    log(
+      stringify({
+        WALLET_ADDRESS,
+        GIVEN_NAME,
+        FAMILY_NAME,
+        IMAGE_FILE,
+      })
+    );
+
+    const identityTokenIssuer = getKeypairFromString(identityTokenSecretKey);
+
+    const uploadedImageUrl = await uploadImageToArweave(IMAGE_FILE);
+
+    log(`🖼️ Uploaded image`, uploadedImageUrl);
+
+    const transactionId = await mintAndTransferIdentityToken(
+      WALLET_ADDRESS,
+      GIVEN_NAME,
+      FAMILY_NAME,
+      uploadedImageUrl,
+      identityTokenIssuer
+    );
+    log(transactionId);
+
+    log(`✅ Completed successfully`);
+  },
+  30 * SECONDS
+);
