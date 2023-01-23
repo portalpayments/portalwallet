1689b6c00833a6db43638e64b93b08fe1f570d35
Mike MacCana <mike.maccana@gmail.com>
2023-01-23 23:17:23 +0000

    Fix 'scroll to bottom' not disappearing

--- src/lib/dom-utils.ts
+++ src/lib/dom-utils.ts
@@ -1,7 +1,10 @@
+import { log, stringify } from "../backend/functions";
+
 export const checkIfScrolledAllTheWay = (element: svelte.JSX.Element) => {
   // Round because sometimes an element that's 70 pixels high can be scrolled 69.7 pixels down (and that's 'all the way')
   const howFarScrolled = Math.round(element.scrollTop + element.clientHeight);
   const scrollableArea = element.scrollHeight;
-  const hasScrolledAllTheWay = howFarScrolled === scrollableArea;
+  // And because of above, occasionally a 100 pixel element can be scrolled 101 pixels, so allow 'greater than' too.
+  const hasScrolledAllTheWay = howFarScrolled >= scrollableArea;
   return hasScrolledAllTheWay;
 };
