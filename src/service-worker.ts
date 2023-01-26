82bfd2d50c5903ecb304de13f5b5824a7dd05d52
Mike MacCana <mike.maccana@gmail.com>
2023-01-26 19:18:22 +0000

    Move cacheWebRequests() into seperate file

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -11,6 +11,7 @@
 // https://stackoverflow.com/questions/62619058/appending-js-extension-on-relative-import-statements-during-typescript-compilat
 import type { AccountSummary, Contact } from "./backend/types.js";
 import { log } from "./backend/functions.js";
+import { cacheWebRequests } from "./service-worker-webcache";
 import * as localforage from "localforage";
 
 const VERSION = 23;
@@ -135,60 +136,5 @@ self.addEventListener("message", (event) => {
 // Cache GET requests for images
 // Based on https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent
 self.addEventListener("fetch", (event) => {
-  // Let the browser do its default handling for non-GET requests.
-  // @ts-ignore see top of file
-  if (event.request.method !== "GET") {
-    return;
-  }
-
-  // @ts-ignore see top of file
-  const url = event?.request?.url;
-
-  // Not all events have urls. Some are just
-  //  { "isTrusted": true }
-  // Yes really, that's all
-  if (!url) {
-    return;
-  }
-
-  // Let the browser do its default handling for non-Arweave requests.
-  // @ts-ignore see top of file
-
-  if (!url.startsWith("https://arweave.net")) {
-    return;
-  }
-
-  // Prevent the default, and handle the request ourselves.
-  // @ts-ignore see top of file
-  event.respondWith(
-    (async () => {
-      // @ts-ignore see top of file
-      // log("🖼️: Using Service Worker for Arweave request", event.url);
-      // @ts-ignore see top of file
-      const eventRequest = event.request;
-      // Try to get the response from a cache.
-      const cache = await caches.open(`portal-v${VERSION}`);
-      // @ts-ignore see top of file
-      const cachedResponse = await cache.match(eventRequest);
-
-      if (cachedResponse) {
-        // If we found a match in the cache, return it.
-
-        // log(
-        //   // "✨: Using Service Worker cached response for Arweave request",
-        //   // @ts-ignore see top of file
-        //   url
-        // );
-        return cachedResponse;
-      }
-
-      // If we didn't find a match in the cache, use the network.
-      // @ts-ignore see top of file
-      log("⛔: No Service Worker cached item for Arweave request", url);
-      const response = await fetch(eventRequest);
-      log(`🔜: Fetched response for ARweave and added it to cache`);
-      cache.put(url, response.clone());
-      return response;
-    })()
-  );
+  return cacheWebRequests(event, VERSION);
 });
