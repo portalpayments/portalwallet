abdce24ec7484167469e645405211edde9e9c325
Mike MacCana <mike.maccana@gmail.com>
2023-01-30 21:42:03 +0000

    Remove fixed todo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import "./app.scss";
 import "normalize.css";
 import "events";
-// @ts-ignore TS reports an error here but everything works fine
-// and the file definitely exists
 import App from "./App.svelte";
 
 const app = new App({
