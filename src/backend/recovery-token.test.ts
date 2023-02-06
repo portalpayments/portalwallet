85251fb706f6e31cab167ec73b40da346aca68d4
Mike MacCana <mike.maccana@gmail.com>
2023-02-06 16:27:39 +0000

    Clean up traditional recovery code

--- src/backend/recovery-token.test.ts
+++ src/backend/recovery-token.test.ts
@@ -7,6 +7,7 @@
 // You should have received a copy of the GNU General Public License along with Portal Wallet. If not, see <https://www.gnu.org/licenses/>.
 //
 import type { Connection, Keypair } from "@solana/web3.js";
+import { Keypair as KeypairConstructor } from "@solana/web3.js";
 import { log } from "./functions";
 import {
   personalPhraseToEntropy,
@@ -20,6 +21,7 @@ import { connect, getAccountBalance, putSolIntoWallet } from "./wallet";
 import * as dotenv from "dotenv";
 import * as base58 from "bs58";
 import { expectedCleanedPersonalPhrase } from "./phrase-cleaning.test";
+import * as bip39 from "bip39";
 
 jest.mock("./functions");
 
@@ -31,59 +33,41 @@ const password = `${new Date().toString()}`;
 
 dotenv.config();
 
-describe(`restoration`, () => {
+describe(`traditional keypair creation and restoration`, () => {
   let connection: Connection;
-  let keypairs: Array<Keypair>;
-  let restoredKeypairs: Array<Keypair>;
+  const mnemonic = bip39.generateMnemonic();
+  let originalWallet: Keypair;
+  let restoredWallet: Keypair;
+
   beforeAll(async () => {
     connection = await connect("localhost");
   });
 
-  test(
-    `wallets can be created using randomness and nmemonics`,
-    async () => {
-      const entropy = await personalPhraseToEntropy(
-        expectedCleanedPersonalPhrase,
-        fullName
-      );
-      const mnemonic = entropyToMnemonic(entropy);
-      keypairs = await mnemonicToKeypairs(mnemonic, password);
+  test(`wallets can be created from a mnemonic`, async () => {
+    const seed = bip39.mnemonicToSeedSync(mnemonic, password);
+    originalWallet = KeypairConstructor.fromSeed(seed.slice(0, 32));
 
-      // IMPORTANT: if we don't deposit any Sol the wallet won't exist
-      const firstWallet = keypairs[0];
-      await putSolIntoWallet(connection, firstWallet.publicKey, DEPOSIT);
+    // IMPORTANT: if we don't deposit any Sol the wallet won't exist
+    await putSolIntoWallet(connection, originalWallet.publicKey, DEPOSIT);
 
-      const accountBalance = await getAccountBalance(
-        connection,
-        firstWallet.publicKey
-      );
-      expect(accountBalance).toEqual(DEPOSIT);
-    },
-    // TODO: this is an absurdly log test but we're about to delete the code
-    30 * SECONDS
-  );
+    const accountBalance = await getAccountBalance(
+      connection,
+      originalWallet.publicKey
+    );
+    expect(accountBalance).toEqual(DEPOSIT);
+  });
 
-  test(
-    `wallets can be restored using their seed phrases`,
-    async () => {
-      // Lets re-make the keypairs from the seed
-      const entropy = await personalPhraseToEntropy(
-        expectedCleanedPersonalPhrase,
-        fullName
-      );
-      const mnemonic = entropyToMnemonic(entropy);
-      restoredKeypairs = await mnemonicToKeypairs(mnemonic, password);
+  test(`wallets can be restored using their seed phrases`, async () => {
+    // Lets re-make the keypairs from the seed
 
-      const originalWallet = keypairs[0];
-      const restoredWallet = restoredKeypairs[0];
-      expect(restoredWallet.secretKey).toEqual(originalWallet.secretKey);
-      expect(restoredWallet.publicKey.toBase58()).toEqual(
-        originalWallet.publicKey.toBase58()
-      );
-    },
-    // TODO: this is an absurdly log test but we're about to delete the code
-    30 * SECONDS
-  );
+    const restoredKeypairs = await mnemonicToKeypairs(mnemonic, password);
+
+    restoredWallet = restoredKeypairs[0];
+    expect(restoredWallet.secretKey).toEqual(originalWallet.secretKey);
+    expect(restoredWallet.publicKey.toBase58()).toEqual(
+      originalWallet.publicKey.toBase58()
+    );
+  });
 });
 
 describe(`recovery`, () => {
