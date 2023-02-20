37465321deb2528be85dbc42e9276f908a1d9542
Mike MacCana <mike.maccana@gmail.com>
2023-02-20 16:43:50 +0000

    Revert "Revert "Fix Dialect imports""
    This reverts commit d98276b9167c544540909c770b7c8e04aca8b164.

--- src/backend/messaging.ts
+++ src/backend/messaging.ts
@@ -7,18 +7,20 @@
 // You should have received a copy of the GNU General Public License along with Portal Wallet. If not, see <https://www.gnu.org/licenses/>.
 //
 // From https://docs.dialect.to/documentation/messaging/typescript/configuration
-import type {
+import {
+  Dialect,
   DialectSdk,
-  CreateThreadCommand,
-  DialectCloudEnvironment,
-  SendMessageCommand,
-  Thread,
+  ThreadMemberScope,
+  type CreateThreadCommand,
+  type DialectCloudEnvironment,
+  type SendMessageCommand,
+  type Thread,
 } from "@dialectlabs/sdk";
-import Dialect from "@dialectlabs/sdk";
-import ThreadMemberScope from "@dialectlabs/sdk";
-import type { Solana } from "@dialectlabs/blockchain-sdk-solana";
-import NodeDialectSolanaWalletAdapter from "@dialectlabs/blockchain-sdk-solana";
-import SolanaSdkFactory from "@dialectlabs/blockchain-sdk-solana";
+import {
+  SolanaSdkFactory,
+  NodeDialectSolanaWalletAdapter,
+  type Solana,
+} from "@dialectlabs/blockchain-sdk-solana";
 import type { Keypair } from "@solana/web3.js";
 import type { SimpleTransaction, SimpleWalletMessage } from "./types";
 
