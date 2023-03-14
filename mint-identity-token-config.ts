15c8ad278d8fdbafde0673bcee10a1c1ef0a6d76
Mike MacCana <mike.maccana@gmail.com>
2023-03-14 22:10:34 +0000

    Clean up token minting configs

--- mint-identity-token-config.ts
+++ mint-identity-token-config.ts
@@ -1,22 +1,55 @@
-import { VerifiedClaimsForIndividual } from "./src/backend/types";
+import {
+  VerifiedClaimsForIndividual,
+  VerifiedClaimsForOrganization,
+} from "./src/backend/types";
 
-const WALLET_ADDRESS = "2EfiRdR97jnSknN7KiVwGqXATvuSgGiTypMdiwXxBbxh";
-const GIVEN_NAME = "Joseph";
-const FAMILY_NAME = "McCann";
-const INDIVIDUAL_OR_ORGANIZATION_IMAGE_FILE = "joe.jpg";
-const COVER_IMAGE_FILE = "identity-token-cover-image-for-joe.png";
+// ------------------------------------------------------------------------------
+// CONFIGURE TOKEN FOR AN INDIVIDUAL
+
+// const WALLET_ADDRESS = "2EfiRdR97jnSknN7KiVwGqXATvuSgGiTypMdiwXxBbxh";
+// const GIVEN_NAME = "Joseph";
+// const FAMILY_NAME = "McCann";
+// const INDIVIDUAL_OR_ORGANIZATION_IMAGE_FILE = "joe.jpg";
+// const COVER_IMAGE_FILE = "identity-token-cover-image-for-joe.png";
+
+// const ALREADY_UPLOADED_INDIVIDUAL_OR_ORGANIZATION_IMAGE = null;
+
+// const ALREADY_UPLOADED_COVER_IMAGE = null;
+
+// // Partial because we haven't filled in image URL yet
+// const tokenClaimsNoImageUrl: Omit<VerifiedClaimsForIndividual, "imageUrl"> = {
+//   type: "INDIVIDUAL",
+//   givenName: GIVEN_NAME,
+//   familyName: FAMILY_NAME,
+// };
+
+// ------------------------------------------------------------------------------
+// CONFIGURE TOKEN FOR AN ORGANIZATION
+
+const WALLET_ADDRESS = "45xu19B21vnDMWGWb4M57Yu9D4xZfx5KYroVGhb5RnAF";
+const LEGAL_NAME = "Helius Blockchain Technologies, Inc.";
+const STATE = "California";
+const COUNTRY = "United States of America";
+const JURISDICTION = "State";
+const INDIVIDUAL_OR_ORGANIZATION_IMAGE_FILE = "helius.jpg";
+const COVER_IMAGE_FILE = "identity-token-cover-image-for-helius.png";
+const IS_NOTABLE = true;
 
 const ALREADY_UPLOADED_INDIVIDUAL_OR_ORGANIZATION_IMAGE = null;
-
 const ALREADY_UPLOADED_COVER_IMAGE = null;
 
 // Partial because we haven't filled in image URL yet
-const tokenClaimsNoImageUrl: Omit<VerifiedClaimsForIndividual, "imageUrl"> = {
-  type: "INDIVIDUAL",
-  givenName: GIVEN_NAME,
-  familyName: FAMILY_NAME,
+const tokenClaimsNoImageUrl: Omit<VerifiedClaimsForOrganization, "imageUrl"> = {
+  type: "ORGANIZATION",
+  legalName: LEGAL_NAME,
+  state: STATE,
+  jurisdiction: JURISDICTION,
+  country: COUNTRY,
+  isNotable: IS_NOTABLE,
 };
 
+// ------------------------------------------------------------------------------
+// No need to change anything below this line
 export const config = {
   walletAddress: WALLET_ADDRESS,
   individualOrOrganizationImageFile: INDIVIDUAL_OR_ORGANIZATION_IMAGE_FILE,
@@ -26,32 +59,3 @@ export const config = {
   alreadyUploadedCoverImage: ALREADY_UPLOADED_COVER_IMAGE,
   tokenClaimsNoImageUrl,
 };
-
-// const WALLET_ADDRESS = "7bz4h6T4p39ozCnz7vpDmmiFNssyV1d1Hu3TKcDXmSXu";
-// const LEGAL_NAME = "Bridgesplit, Inc.";
-// const STATE = "Delaware";
-// const COUNTRY = "United States of America";
-// const JURISDICTION = "State";
-// const INDIVIDUAL_OR_ORGANIZATION_IMAGE_FILE = "bridgesplit.jpg";
-// const COVER_IMAGE_FILE = "identity-token-cover-image-for-bridgesplit.png";
-// const IS_NOTABLE = true;
-
-// const ALREADY_UPLOADED_INDIVIDUAL_OR_ORGANIZATION_IMAGE =
-//   "https://gateway.pinata.cloud/ipfs/QmYtohFcvN3DT9pdEofWd7meYA87gRpFnBRAKgRPMp5XBW";
-// const ALREADY_UPLOADED_COVER_IMAGE = null;
-
-// // Partial because we haven't filled in image URL yet
-// const tokenClaimsNoImageUrl: Omit<VerifiedClaimsForOrganization, "imageUrl"> = {
-//   type: "ORGANIZATION",
-//   legalName: LEGAL_NAME,
-//   state: STATE,
-//   jurisdiction: JURISDICTION,
-//   country: COUNTRY,
-//   isNotable: IS_NOTABLE,
-// };
-
-// No need to change anything below this line
-// ------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------
-// ------------------------------------------------------------------------------
