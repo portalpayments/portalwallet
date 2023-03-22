03d45d6312dbab94d3e90fe2eb4521fb526f9c0b
Mike MacCana <mike.maccana@gmail.com>
2023-03-22 14:46:44 +0000

    Logging and cleanups to dApp support

--- src/injected.ts
+++ src/injected.ts
@@ -2,23 +2,23 @@
 // From https://github.com/solana-labs/wallet-standard/blob/master/WALLET.md
 
 import { log, stringify } from "./backend/functions";
-import { PublicKey } from "@solana/web3.js";
 import { registerWallet } from "./lib/wallet-standard-adapter/register";
 import { PortalWalletStandardImplementation } from "./lib/wallet-standard-adapter/wallet-standard";
 
-log("Registering wallet implementation");
-
-// Register your wallet implementation using the Wallet Standard, passing the reference.
-registerWallet(PortalWalletStandardImplementation);
-
-// Attach the wallet implementation to the window, guarding against errors.
 try {
+  log("Registering wallet implementation...");
+  registerWallet(PortalWalletStandardImplementation);
+  log("✅ success registering wallet implementation");
+
+  log("Attached Portal wallet implementation to window");
   Object.defineProperty(window, "portal", {
     value: PortalWalletStandardImplementation,
   });
-  log("Attached wallet implementation to window");
+  log("✅ success attaching Portal wallet implementation to window");
 } catch (thrownObject) {
   const error = thrownObject as Error;
   console.error(error);
-  log(`Failed to attach wallet implementation to window: ${stringify(error)}`);
+  log(
+    `🛑 Failed to attach wallet implementation to window: ${stringify(error)}`
+  );
 }
