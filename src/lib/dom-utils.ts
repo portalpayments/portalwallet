d15fa9dc60bb586a1a4e59e31661c0acf6d8257e
Mike MacCana <mike.maccana@gmail.com>
2023-03-27 17:01:13 +0100

    Fix TS warning

--- src/lib/dom-utils.ts
+++ src/lib/dom-utils.ts
@@ -13,6 +13,9 @@ import { log, stringify } from "../backend/functions";
 // In pixels
 const leeWay = 50;
 
+// Workaround for "TS2503: Cannot find namespace 'svelte'." during npm run build:content-script
+// TODO: find correct settings for rollup config to fix this
+// @ts-ignore
 export const checkIfScrolledAllTheWay = (element: svelte.JSX.Element) => {
   // Round because sometimes an element that's 70 pixels high can be scrolled 69.7 pixels down (and that's 'all the way')
   const howFarScrolled = Math.round(element.scrollTop + element.clientHeight);
