f6767b1d18f6ca46bab0d6925253bcbfbcd832ff
Mike MacCana <mike.maccana@gmail.com>
2023-04-06 13:31:43 +0100

    Just remove openHandlesTimeout from jest completely, we don't seem to be getting issues anymore.

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,8 @@
 import type { Config } from "jest";
-import { SECONDS } from "./src/backend/constants";
 
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-
-  // https://jestjs.io/docs/configuration#openhandlestimeout-number
-  openHandlesTimeout: 2 * SECONDS,
 
   // From https://stackoverflow.com/questions/58603201/jest-cannot-load-svg-file
   transform: {
