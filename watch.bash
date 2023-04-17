787319a7e235bdeb26a49272125aa704ce746eaf
Mike MacCana <mike.maccana@gmail.com>
2023-04-17 15:49:52 +0100

    Tighten watching script

--- watch.bash
+++ watch.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo "⏳ Waiting for changes..."
 DIR_TO_WATCH='src/wallet-standard-adapter/'
-while inotifywait -qqre "attrib,modify,close_write,move,move_self,create,delete,delete_self" "$DIR_TO_WATCH"; do
+CHANGES_TO_WATCH='attrib,modify,close_write,move,move_self,create,delete,delete_self'
+while inotifywait -qqre "$CHANGES_TO_WATCH" "$DIR_TO_WATCH"; do
   echo "🟢 Building..."
-  npm run build:content-script 
+  npm run build:wallet-standard-adapter 
   echo "✅ Done"
 done
