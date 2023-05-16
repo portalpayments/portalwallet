ef1a66e5de88dad3c090102ecf5f710b1f00fbcd
Mike MacCana <mike.maccana@gmail.com>
2023-05-16 12:19:11 +0100

    Formatting only

--- src/wallet-standard-adapter/injected.ts
+++ src/wallet-standard-adapter/injected.ts
@@ -1,6 +1,5 @@
 // Register our wallet implementation with the wallet standard, and attach it to the window so dapps can find it.
 // From https://github.com/solana-labs/wallet-standard/blob/master/WALLET.md
-
 import { log, stringify } from "../backend/functions";
 import { registerWallet } from "./register";
 import { PortalWalletStandardImplementation } from "./wallet-standard";
@@ -18,7 +17,5 @@ try {
 } catch (thrownObject) {
   const error = thrownObject as Error;
   console.error(error);
-  log(
-    `🛑 Failed to attach wallet implementation to window: ${stringify(error)}`
-  );
+  log(`🛑 Failed to attach wallet implementation to window: ${stringify(error)}`);
 }
