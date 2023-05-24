25157d983c795c230227d9d418c135e965b923b2
Mike MacCana <mike.maccana@gmail.com>
2023-05-24 18:27:25 +0100

    Remove window global silliness

--- src/wallet-standard-adapter/injected.ts
+++ src/wallet-standard-adapter/injected.ts
@@ -6,21 +6,15 @@ import { portalWalletStandardImplementation } from "./wallet-standard";
 
 const main = async () => {
   try {
-    log("Registering wallet implementation...");
     registerWallet(portalWalletStandardImplementation);
-    log("✅ success registering wallet implementation");
-    // TODO: do we actually need this?
-    // check at docs link above
-    // it seems a bad idea to create arbitrary window globals
-    log("Attaching Portal wallet implementation to window...");
-    Object.defineProperty(window, "portal", {
-      value: portalWalletStandardImplementation,
-    });
-    log("✅ success attaching Portal wallet implementation to window");
+    log("✅ Registered Portal wallet standard implementation");
+    // https://github.com/solana-labs/wallet-standard/blob/master/WALLET.md
+    // says we should make a global underneath window but that seems gross
+    //
   } catch (thrownObject) {
     const error = thrownObject as Error;
     console.error(error);
-    log(`🛑 Failed to attach wallet implementation to window: ${stringify(error)}`);
+    log(`🛑 Failed to register wallet: ${stringify(error)}`);
   }
 };
 
