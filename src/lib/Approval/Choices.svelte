66445ce73be33d228257e8ce35430b0ccf2a0c52
Mike MacCana <mike.maccana@gmail.com>
2023-05-25 19:19:54 +0100

    Remove last CSS gradient border hack

--- src/lib/Approval/Choices.svelte
+++ src/lib/Approval/Choices.svelte
@@ -6,64 +6,49 @@
 </script>
 
 <div class="choices">
-  <div class="rounded-gradient-border-hack decline">
-    <button on:click={onDecline}>
-      <!-- Since the previous hack needs a white background, we need another div to the 'background as gradient text' hack -->
-      <div class="gradient-text-hack">{declineLabel}</div></button
-    >
-  </div>
-
-  <div class="rounded-gradient-border-hack agree">
-    <button on:click={onApprove}>{approveLabel}</button>
-  </div>
+  <button class="decline" on:click={onDecline}> {declineLabel}</button>
+  <button class="agree" on:click={onApprove}>{approveLabel}</button>
 </div>
 
 <style lang="scss">
   @import "../../mixins.scss";
 
   .choices {
-    display: grid;
     grid-auto-flow: column;
     gap: 12px;
     grid-template-columns: 1fr 1fr;
   }
 
-  .rounded-gradient-border-hack {
-    display: grid;
-    align-content: center;
-    padding: 0.5px;
-    width: 100%;
-    height: 36px;
+  // .rounded-gradient-border-hack {
+  //   align-content: center;
+  //   padding: 0.5px;
+  //   width: 100%;
+  //   height: 36px;
 
-    border-radius: 24px;
-    place-items: center;
-    @include shadow;
-  }
+  //   border-radius: 24px;
+  //   place-items: center;
+  //   @include shadow;
+  // }
 
-  // TODO: we don't want to prefer either button
-  // maybe make both buttons white (so neither is 'active')?
+  // // TODO: we don't want to prefer either button
+  // // maybe make both buttons white (so neither is 'active')?
 
-  .rounded-gradient-border-hack.decline {
-    padding: 0.5px;
-    background: var(--blue-green-gradient);
-  }
+  // .rounded-gradient-border-hack.decline {
+  //   padding: 0.5px;
+  //   background: var(--blue-green-gradient);
+  // }
 
-  .rounded-gradient-border-hack.decline button {
-    background: var(--white);
-  }
+  // .rounded-gradient-border-hack.decline button {
+  //   background: var(--white);
+  // }
 
-  .rounded-gradient-border-hack.decline button .gradient-text-hack {
-    @include blue-green;
-  }
-  .rounded-gradient-border-hack.agree {
-    background: var(--blue-green-gradient);
-    color: var(--white);
-  }
-
-  .rounded-gradient-border-hack.agree button {
-    color: var(--white);
-    background: transparent;
-  }
+  // .rounded-gradient-border-hack.decline button .gradient-text-hack {
+  //   @include blue-green;
+  // }
+  // .rounded-gradient-border-hack.agree {
+  //   background: var(--blue-green-gradient);
+  //   color: var(--white);
+  // }
 
   button {
     display: grid;
@@ -75,5 +60,16 @@
     width: 100%;
     // 35px / 2
     border-radius: 17.5px;
+  }
+
+  button.decline {
+    color: var(--mid-blue);
+    @include gradient-border(0.5px);
+  }
+
+  button.agree {
+    color: var(--white);
+    background: var(--blue-green-gradient);
+    @include shadow;
   }
 </style>
